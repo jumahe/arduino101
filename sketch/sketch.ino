865084ad6f1208ebedde1de3a513ea8e8ef0391d
jumahe <jumahe@gmail.com>
2016-09-13 14:23:59 +0200

    an empty sketch

---
+++ sketch/sketch.ino
@@ -0,0 +1,28 @@
+// ICI importez les bonnes librairies
+
+// ICI déclarez vos variables et constantes
+// - l'URL du service
+// - la query (qui sera à concaténer à la suite de l'URL)
+// - un client HTTP (pour interroger le service)
+// - la pin de la LED
+// - le buffer de données réceptionnées
+// - des variables complémentaires selon vos besoins
+
+// SETUP (appelé une seule fois)
+void setup()
+{
+  // initialisez le Serial
+  // initialisez le Bridge
+  // initialisez la pin de la LED
+}
+
+// LOOP (appelé en boucle)
+void loop()
+{
+  // RAZ du buffer
+  // envoi de la requête
+  // lecture de la réponse (caractère à caractère)
+  // traitement de la réponse
+  // notification ?
+  // temporisation (une pause avant la prochaine requête)
+}
